89e1cd5d9fc0545b93fb69cf9d031a99a55e8ed3
Fernando Cruz <cruzfernandomartin94@gmail.com>
2022-08-20 00:28:30 -0300

    flex wrap added to the deck page. TO DO: improve styling, finish the card functionality

--- src/components/deck/Deck.js
+++ src/components/deck/Deck.js
@@ -8,25 +8,30 @@ export default function Deck() {
   return (
     <div>
       <h2>I'm deck</h2>
-      {deck.map((card) => {
-        return (
-          <div className="card">
-            <h4>{card.name}</h4>
-            <img src={`${card.card_images[0].image_url}`} alt="Card" />
-            <div>
-              <p>{card.desc}</p>
+      <div className="containerCards">
+        {deck.map((card) => {
+          return (
+            <div className="card">
+              <h4>{card.name}</h4>
+              <img src={`${card.card_images[0].image_url}`} alt="Card" />
+              <div>
+                <p>{card.desc}</p>
+              </div>
+              <div>
+                <button>
+                  <Link to={`/details/${card.id}`}>Details</Link>
+                </button>
+                <button onClick={() => console.log("we should delete this!")}>
+                  Delete me!
+                </button>
+              </div>
             </div>
-            <div>
-              <button>
-                <Link to={`/details/${card.id}`}>Details</Link>
-              </button>
-              <button onClick={() => console.log("we should delete this!")}>
-                Delete me!
-              </button>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
+      {/* <aside>
+        Let's see what happens
+      </aside> */}
     </div>
   );
 }
--- src/context/DeckContext.js
+++ src/context/DeckContext.js
@@ -5,8 +5,17 @@ const DeckContext = createContext();
 export function DeckProvider ( { children } ) {
 
     const [deck, setDeck] = useState([])
+    
+
 
     const handleAddToDeck = (cardToAdd) => {
+        // let sameCardsInDeck = deck.filter( item => item === cardToAdd)
+        // console.log(sameCardsInDeck)
+        // console.log(deck)
+        
+
+        // TO DO: probar con mover la funcion de chequeo de si la carta está en deck al scope de Declkist
+        //Quizás eso sea lo que hace que en cada renderizado vuelva a ver los arrays vacíos.
         setDeck(prevDeck => [...prevDeck, cardToAdd])
     }
 
