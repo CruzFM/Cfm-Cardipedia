42d030d17e8deb623a2f46d3d0d800333e854b57
Fernando Cruz <cruzfernandomartin94@gmail.com>
2022-08-27 00:10:21 -0300

    minor styles changes in home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 export default function Home(){
 
     return(
-        <div className='home flex-column justify-center align-items-center'>
-            <h1>Welcome to the YUGIOH card pool!</h1>
-            <h2>Where you can seach your favorite cards from a vast database provided by YGOpro</h2>
+        <div className='home flex-column justify-center align-items-center to-center'>
+            <h2 className='text-center'>Welcome to the YUGIOH card pool!</h2>
+            <h3 className='text-center'>Where you can seach your favorite cards from a vast database provided by YGOpro</h3>
             <Link to='/cards/'>To Card pool</Link>
             <Link to='/deck'>To deck</Link>
         </div>
